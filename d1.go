0b2e76ee5af9d2c7fcb399ef28e39ebf760e8647
Mikolaj Figurski <meek.f80@gmail.com>
2022-12-06 15:28:06 -0500

    Do d1 pt 2

--- d1.go
+++ d1.go
@@ -17,29 +17,56 @@ func do_by_line(f *os.File, fn func(string)) {
 	}
 }
 
-func main() {
+func find_top_k(l []int, k int) []int {
+	// find top k by partitioning
+	pivot := l[0]
+	left := make([]int, 0)
+	right := make([]int, 0)
+	for _, v := range l[1:] {
+		if v > pivot {
+			left = append(left, v)
+		} else {
+			right = append(right, v)
+		}
+	}
+	if len(left) == k-1 {
+		return append(left, pivot)
+	}
+	if len(left) > k-1 {
+		return find_top_k(left, k)
+	}
+	return append(left, append([]int{pivot}, find_top_k(right, k-len(left)-1)...)...)
+}
+
+func d1() {
 	fmt.Println("vim-go")
-	// read d1data.txt
+	// open d1data.txt
 	f, err := os.Open("d1data.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
 
-	max := 0
+	// read to get calories being carried
+	carried := make([]int, 0)
 	current := 0
 	do_by_line(f, func(s string) {
 		// convert to int
 		i, err := strconv.Atoi(s)
 		if err != nil {
 			// must be ""
-			if current > max {
-				max = current
-			}
+			carried = append(carried, current)
 			current = 0
+		} else {
+			current += i
 		}
-		current += i
 	})
+	if current != 0 {
+		carried = append(carried, current)
+	}
 
-	fmt.Println(max)
+	// find sum of top 3
+	top3 := find_top_k(carried, 3)
+	sum := top3[0] + top3[1] + top3[2]
+	fmt.Println(sum)
 }
